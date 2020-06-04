56b4e47810a4c6c376c56e073f9fbc321a60dcb5
ben <beneaves01@hotmail.com>
2020-06-04 16:41:15 +0800

    init

--- admin/rout/index.js
+++ admin/rout/index.js
@@ -1,79 +1,31 @@
-const fs = require('fs');
+const fs = require('fs'),
+utils = require('utils');
 
 const rout = {
   create: function(req,res){
-    let body = jp(req.body),
-    title = body.schema.title.replace(/ /g,'_');
-    fs.writeFile('./app/data/feeds/'+ title +'.json', js(body.schema), function(err){
+
+    utils.create(req, function(err, msg){
       if(err){
         res.writeHead(500, { 'Content-Type': 'application/json;'});
-        return res.end(js({status: 'unable to create feed schema'}));
+        return res.end(js({status: err}));
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/json;'});
+        return res.end(js({status: msg}));
       }
-      fs.writeFile('./atom/'+ title +'.xml', body.xml, function(err){
-        if(err){
-          res.writeHead(500, { 'Content-Type': 'application/json;'});
-          return res.end(js({status: 'unable to create feed xml'}));
-        }
-        fs.readdir('./app/data/feeds', function(err, files){
-          if(err){
-            res.writeHead(500, { 'Content-Type': 'application/json;'});
-            return res.end(js({status: 'unable to check feed-list'}));
-          }
-          let arr = [];
-          for (let i = 0; i < files.length; i++) {
-            if(files[i] !== '.gitkeep'){
-              arr.push(files[i].split('.')[0].replace(/ /g,'_'))
-            }
-          }
-          fs.writeFile('./app/data/feed_list.json', js(arr), function(err){
-            if(err){
-              res.writeHead(500, { 'Content-Type': 'application/json;'});
-              return res.end(js({status: 'unable to update feed list'}));
-            }
-            res.writeHead(200, { 'Content-Type': 'application/json;'});
-            return res.end(js({status: title + ' feed created'}));
-          })
-        })
-      })
     })
+
   },
   delete: function(req,res){
-    let body = jp(req.body),
-    title = body.title.replace(/ /g,'_');
 
-    fs.unlink('./app/data/feeds/'+ title +'.json', function(err){
+    utils.delete(req, function(err, msg){
       if(err){
         res.writeHead(500, { 'Content-Type': 'application/json;'});
-        return res.end(js({status: 'unable to delete feed'}));
+        return res.end(js({status: err}));
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/json;'});
+        return res.end(js({status: msg}));
       }
-      fs.unlink('./atom/'+ title +'.xml', function(err){
-        if(err){
-          res.writeHead(500, { 'Content-Type': 'application/json;'});
-          return res.end(js({status: 'unable to delete ./atom/'+ title +'.xml'}));
-        }
-        fs.readdir('./app/data/feeds', function(err, files){
-          if(err){
-            res.writeHead(500, { 'Content-Type': 'application/json;'});
-            return res.end(js({status: 'unable to check feed-list'}));
-          }
-          let arr = [];
-          for (let i = 0; i < files.length; i++) {
-            if(files[i] !== '.gitkeep'){
-              arr.push(files[i].split('.')[0].replace(/ /g,'_'))
-            }
-          }
-          fs.writeFile('./app/data/feed_list.json', js(arr), function(err){
-            if(err){
-              res.writeHead(500, { 'Content-Type': 'application/json;'});
-              return res.end(js({status: 'unable to update feed list'}));
-            }
-            res.writeHead(200, { 'Content-Type': 'application/json;'});
-            return res.end(js({status: title + ' deleted'}));
-          })
-        })
-      })
     })
-
   }
 }
 
---
+++ admin/utils/index.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+const utils = {
+  create: function(req, cb){
+    let body = jp(req.body),
+    title = body.schema.title.replace(/ /g,'_');
+    fs.writeFile('./app/data/feeds/'+ title +'.json', js(body.schema), function(err){
+      if(err){
+        return cb('unable to create feed schema');
+      }
+      fs.writeFile('./atom/'+ title +'.xml', body.xml, function(err){
+        if(err){
+          return cb('unable to create feed xml');
+        }
+        fs.readdir('./app/data/feeds', function(err, files){
+          if(err){
+            return cb('unable to check feed-list');
+          }
+          let arr = [];
+          for (let i = 0; i < files.length; i++) {
+            if(files[i] !== '.gitkeep'){
+              arr.push(files[i].split('.')[0].replace(/ /g,'_'))
+            }
+          }
+          fs.writeFile('./app/data/feed_list.json', js(arr), function(err){
+            if(err){
+              return cb('unable to update feed list');
+            }
+            return cb(false, title + ' feed created');
+          })
+        })
+      })
+    })
+  },
+  delete: function(req, cb){
+    let body = jp(req.body),
+    title = body.title.replace(/ /g,'_');
+
+    fs.unlink('./app/data/feeds/'+ title +'.json', function(err){
+      if(err){
+        return cb('unable to delete feed');
+      }
+      fs.unlink('./atom/'+ title +'.xml', function(err){
+        if(err){
+          return cb('unable to delete ./atom/'+ title +'.xml');
+        }
+        fs.readdir('./app/data/feeds', function(err, files){
+          if(err){
+            return cb('unable to check feed-list');
+          }
+          let arr = [];
+          for (let i = 0; i < files.length; i++) {
+            if(files[i] !== '.gitkeep'){
+              arr.push(files[i].split('.')[0].replace(/ /g,'_'))
+            }
+          }
+          fs.writeFile('./app/data/feed_list.json', js(arr), function(err){
+            if(err){
+              return cb('unable to update feed list');
+            }
+            return cb(false, title + ' deleted');
+          })
+        })
+      })
+    })
+  }
+}
+
+module.exports = utils;
