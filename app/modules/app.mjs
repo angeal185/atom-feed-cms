7101c1facdc655c845626b34b0c7477eee0c9642
ben <beneaves01@hotmail.com>
2020-06-05 01:12:05 +0800

    init

--- app/modules/app.mjs
+++ app/modules/app.mjs
@@ -1,13 +1,14 @@
+window.eval = null;
 window.js = JSON.stringify;
 window.jp = JSON.parse;
 window.cl = console.log;
 window.ce = console.error;
 
+
 import { config } from './config.mjs';
 import { h } from './h.mjs';
 import { utils } from './utils.mjs';
 import { tpl } from "./tpl.mjs";
-
 
 window.onload = function(){
   let win = window,
